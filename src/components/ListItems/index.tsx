d2e290a399875181d852e979e843a45015de88e7
marioq <mario300@gmail.com>
2022-08-25 01:01:06 -0300

    chore: add searchbar function

--- src/components/ListItems/index.tsx
+++ src/components/ListItems/index.tsx
@@ -1,25 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './styles.scss'
 
 import ReactPaginate from 'react-paginate'
 import { Items } from './Items'
+import { Store } from '../../Store'
 
 export const ListItems = ({ apiData }:{apiData:any}) => {
-  // const [list, setList] = useState<any>([])
+  const [list, setList] = useState<any>([])
 
   const [currentItems, setCurrentItems] = useState(null)
   const [pageCount, setPageCount] = useState<number>(0)
   const [itemOffset, setItemOffset] = useState<number>(0)
   const itemsPerPage = 8
 
+  const { searchBy } = useContext(Store)
+
   useEffect(() => {
+    if (searchBy !== '') {
+      const regexp = new RegExp(searchBy, 'i')
+      const filteredItems = apiData.filter((x:any) => regexp.test(x.name))
+      setList(filteredItems)
+      console.log(filteredItems.length)
+      if (!filteredItems.length) {
+        setCurrentItems(null)
+        return
+      }
+    } else {
+      setList(apiData)
+    }
     const endOffset = itemOffset + itemsPerPage
-    setCurrentItems(apiData.slice(itemOffset, endOffset))
-    setPageCount(Math.ceil(apiData.length / itemsPerPage))
-  }, [apiData, itemOffset, itemsPerPage])
+    setCurrentItems(list.slice(itemOffset, endOffset))
+    setPageCount(Math.ceil(list.length / itemsPerPage))
+  }, [apiData, itemOffset, itemsPerPage, searchBy])
 
   const handlePageClick = (event: { selected: number }) => {
-    const newOffset = (event.selected * itemsPerPage) % apiData.length
+    const newOffset = (event.selected * itemsPerPage) % list.length
     setItemOffset(newOffset)
   }
 
@@ -30,6 +45,7 @@ export const ListItems = ({ apiData }:{apiData:any}) => {
           currentItems
             ? (
             <>
+            {console.log(currentItems)}
             <ul className='items__list'>
               <Items currentItems={ currentItems } />
             </ul>
@@ -53,11 +69,14 @@ export const ListItems = ({ apiData }:{apiData:any}) => {
                 nextClassName={'page-item'}
                 nextLinkClassName={'page-link bg-dark text-warning'}
                 activeClassName={'active'}
+                disabledClassName={'text-dark'}
               />
             </nav>
             </>
               )
-            : null
+            : (
+              <h1>No hay resultados</h1>
+              )
         }
       </div>
     </>
