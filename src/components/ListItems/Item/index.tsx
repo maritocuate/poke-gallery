5013ebdabc356b11a4919b5abb1777effca48580
marioq <mario300@gmail.com>
2023-03-20 01:52:58 -0300

    chore: add gsap animations

--- src/components/ListItems/Item/index.tsx
+++ src/components/ListItems/Item/index.tsx
@@ -1,21 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import './styles.scss'
 
 import { Store } from '../../../Store'
+import { gsap, Elastic } from 'gsap'
 
 export const Item = ({ data } : { data:any }) => {
   const { name, weight, sprites } = data
   const { myPokes, updateMyPokes } = useContext(Store)
+  const imgRef = useRef(null)
 
   const checkIfExist = () => !!(myPokes.filter((item:string) => item === name).length)
 
+  const handleImageLoad = () => {
+    gsap.set(imgRef.current, { opacity: 1 })
+    gsap.from(imgRef.current, { opacity: 0, scale: 0.8, duration: 0.8, ease: Elastic.easeOut.config(1, 0.3) })
+  }
+
   return (
-    <li className={`item ${checkIfExist() ? 'disabled' : ''}`}>
-      <div className="card bg-dark item__card">
+    <div className={`item ${checkIfExist() ? 'disabled' : ''}`}>
+      <div className="card bg-dark item__card" ref={imgRef}>
         <img
           className='card-img-top item__image loaded'
-          src={sprites.front_default}
+          // eslint-disable-next-line
+          src={sprites.other["official-artwork"].front_default}
           alt={name}
+          onLoad={handleImageLoad}
         />
         <div className="card-body">
           <p className="card-title">{name.toUpperCase()}</p>
@@ -23,6 +32,6 @@ export const Item = ({ data } : { data:any }) => {
         </div>
       </div>
       <span className='item__hover' onClick={() => !checkIfExist() ? updateMyPokes(name) : null}>+</span>
-    </li>
+    </div>
   )
 }
